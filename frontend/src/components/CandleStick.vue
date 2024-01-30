2760974be22dded8beeedcb3f6a5e9da8ba667e1
VH Phan <phanveehuen@gmail.com>
2024-01-30 16:58:31 +0800

    responsive chart height and width

--- frontend/src/components/CandleStick.vue
+++ frontend/src/components/CandleStick.vue
@@ -2,10 +2,11 @@
 
 import {useMainStore} from "@/store/mainStore.js";
 import {storeToRefs} from "pinia";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 
 const mainStore = useMainStore();
 const {ohlcData} = storeToRefs(mainStore);
+const apexChart = ref(null);
 
 const cleanedData = computed(() => {
       return ohlcData.value.map((item) => {
@@ -29,8 +30,10 @@ const options = {
     tooltip: {
       enabled: true
     }
-  }
+  },
+
 };
+
 const series = computed(() => {
   return [
     {
@@ -40,11 +43,30 @@ const series = computed(() => {
   ];
 });
 
+const chartHeight = `${Math.max(window.innerHeight - 600, 250)}px`;
+
+window.addEventListener('resize', () => {
+      const heightValue = Math.max(window.innerHeight - 600, 250);
+      // chartHeight.value = `${heightValue}px`;
+      apexChart.value.updateOptions({
+        chart: {
+          height: heightValue
+        }
+      });
+    }
+);
+
 </script>
 
 <template>
   <div class="row">
-    <apexchart class="col-12" type="candlestick" :options="options" :series="series"></apexchart>
+    <apexchart class="col-12"
+               type="candlestick"
+               :options="options"
+               :series="series"
+               :height="chartHeight"
+               ref="apexChart"
+    ></apexchart>
   </div>
 
 
